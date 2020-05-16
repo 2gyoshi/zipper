202bd184ba2625d0c460c46511ff3a3c90598036
Yoshifumi-T <2g.4423@gmail.com>
2020-05-17 02:00:19 +0900

    スタイル調整

--- script.js
+++ script.js
@@ -14,15 +14,19 @@ function initializeTriangle() {
     
     if(!body || !triangle) return;
     
-    const size = body.clientWidth / 2;
+    // const size = body.clientWidth / 2;
+    const size = 15;
     triangle.style.borderLeft  = `${size}px solid transparent`;
     triangle.style.borderRight = `${size}px solid transparent`;
+    triangle.style.position    = 'absolute';
+    triangle.style.left = '50%'
+    triangle.style.transform   = `translateX(-${size}px)`;
 }
 
 // レンジスライダーの値を表示する為のイベントを追加する
 function attachEventForRange() {
     const body = document.querySelector('body');
-    const range = document.querySelector('.range-input');
+    const range = document.querySelector('.input-range');
     
     if(!body || !range) return;
     
@@ -36,7 +40,7 @@ function test() {
 }
 
 function test2(event) {
-    const range = document.querySelector('.range-input');
+    const range = document.querySelector('.input-range');
 
     if(!range) return;
 
@@ -47,13 +51,12 @@ function test2(event) {
 }
 
 // マウスにあわせて要素を動かす
-function changeRangeValue(event) {
-    const range = document.querySelector('.range-input');
-    const value = document.querySelector('.range-value');
+// function changeRangeValue(event) {
+//     const range = document.querySelector('.input-range');
 
-    if(!range || !value) return;
+//     if(!range || !value) return;
     
-    const y = event.offsetY;
-    const e = document.querySelector('.range-value');
-    e.innerHTML = `${range.value}px`;
-}
+//     const y = event.offsetY;
+//     const e = document.querySelector('.range-value');
+//     e.innerHTML = `${range.value}px`;
+// }
