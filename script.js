0f8b576bc05b6b51011a5409d0ca0fec8415fa5e
Yoshifumi-T <2g.4423@gmail.com>
2020-05-29 14:52:03 +0900

    スタイル調整

--- script.js
+++ script.js
@@ -3,60 +3,33 @@
 window.onload   = init;
 window.onresize = init;
 
-function init() {
-    initializeTriangle();
-    attachEventForRange();
-}
-
-function initializeTriangle() {
-    const body = document.querySelector('body');
-    const triangle = document.querySelector('.triangle');
-    
-    if(!body || !triangle) return;
-    
-    // const size = body.clientWidth / 2;
-    const size = 15;
-    triangle.style.borderLeft  = `${size}px solid transparent`;
-    triangle.style.borderRight = `${size}px solid transparent`;
-    triangle.style.position    = 'absolute';
-    triangle.style.left = '50%'
-    triangle.style.transform   = `translateX(-${size}px)`;
-}
-
 // レンジスライダーの値を表示する為のイベントを追加する
-function attachEventForRange() {
+function init() {
     const body = document.querySelector('body');
-    const range = document.querySelector('.input-range');
+    const zipper = document.querySelector('.zipper');
+    const elements = document.querySelectorAll('li');
     
-    if(!body || !range) return;
+    if(!body || !zipper || !elements) return;
     
-    range.setAttribute('max', body.clientHeight);
-    range.addEventListener('input', test2);
+    zipper.setAttribute('max', zipper.clientWidth);
+    zipper.addEventListener('input', changeValue);
+
+    const size = zipper.clientWidth / 10;
+    elements.forEach(e => e.style.height = `${size}px`);
 }
 
-function test() {
-    document.addEventListener('mousemove', changeRangeValue);
-    document.addEventListener('mousemove', test2);
+function changeValue() {
+    const zipper = document.querySelector('.zipper');
+    const elements = document.querySelectorAll('li');
+
+    if(!zipper || !elements) return;
+
+    const progress = (zipper.value / zipper.clientWidth) * 100;
+    elements.forEach(e => {
+        if(Number(e.textContent <= progress)){
+            e.classList.add('move');
+        } else {
+            e.classList.remove('move');
+        }
+    });
 }
-
-function test2(event) {
-    const range = document.querySelector('.input-range');
-
-    if(!range) return;
-
-    const y = event.offsetY;
-    const e = document.querySelector('.triangle');
-    // e.style.borderTop = `${y}px solid #b2ce54`;
-    e.style.borderTop = `${range.value}px solid red`;
-}
-
-// マウスにあわせて要素を動かす
-// function changeRangeValue(event) {
-//     const range = document.querySelector('.input-range');
-
-//     if(!range || !value) return;
-    
-//     const y = event.offsetY;
-//     const e = document.querySelector('.range-value');
-//     e.innerHTML = `${range.value}px`;
-// }
